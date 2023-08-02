2ea9ef26bf57a14202f21670031e9d0407dc332f
trdmitr <trdmitr@gmail.com>
2023-08-02 13:07:49 +0400

    css

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -7,7 +7,7 @@ class HomePage extends Component {
 
     render() {
     // const imgUrl2 = "https://sun9-74.userapi.com/impg/Os89_a2-ESXOQM4Nd75Ra9YXsbDx_NW-2HT22g/Q001Xopq5Vk.jpg?size=1280x817&quality=95&sign=86aaf7802eb6e4fb5d44700170d03b3a&type=album"
-    const imgUrl = "https://sun9-57.userapi.com/impg/921gduBgRCQPtvmL8pBJrfgvcywHXeCQuvrcBQ/ZPrG8PgEd88.jpg?size=824x876&quality=96&sign=e7a7f47685d431e1852d410d713930e4&type=album"
+    const imgUrl = "https://sun9-9.userapi.com/impg/kOa-hB3uTos0sAfKYduWN0eXgalllzBTEYhfFQ/D5O2TBUv-14.jpg?size=512x577&quality=96&sign=588d2f107d060c2f18a1c67007b88b77&type=album"
     const imgAlt = "Каверы Подземки"
     // const frontAudio = "https://drive.google.com/uc?export=download&id=1uNmg51HFdA3aI84AtgWYAuM_rTnqoZyS";
     // const imgTzi = "https://sun9-40.userapi.com/impg/MISALnCzbtAXGdxwXPqulc-f9a-JzHxkZHjYnA/UcHxUh-CRM8.jpg?size=600x200&quality=96&sign=c367e03fe8e4d86e0f7fb657fd7ba0e3&type=album"
